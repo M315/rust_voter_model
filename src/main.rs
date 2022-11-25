29324720da9e59bdcd4dbe9c4ad4ddb046ca97b2
marc-tenokonda <106654482+marc-tenokonda@users.noreply.github.com>
2022-11-25 05:46:46 +0100

    Add color and point size

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rand::distributions::{Distribution, Uniform};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::rect::Rect;
 use std::time::Duration;
 
 fn main() -> Result<(), String> {
@@ -12,9 +12,16 @@ fn main() -> Result<(), String> {
         .video()
         .expect("Unable to initialize the video system");
 
+    // Set initial parameters
+    let width : u32 = 200;
+    let height : u32 = 200;
+
+    let w_rec : u32 = 4;
+    let h_rec : u32 = 4;
+
     // Generate a window
     let window = video_subsystem
-        .window("rust-sdl2 demo", 50 * 20, 50 * 20)
+        .window("rust-sdl2 demo", width * w_rec, height * h_rec)
         .position_centered()
         .build()
         .unwrap();
@@ -28,8 +35,8 @@ fn main() -> Result<(), String> {
     // Set color
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     // Paint the hole canvas
-    //canvas.clear();
-    //canvas.present();
+    canvas.clear();
+    canvas.present();
 
     // Initialize the event tracker
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -39,8 +46,10 @@ fn main() -> Result<(), String> {
     let mut rng = rand::thread_rng();
 
     // Initial possition
-    let mut x = 25;
-    let mut y = 25;
+    let mut x: i32 = (width / 2u32) as i32;
+    let mut y: i32 = (height / 2u32) as i32;
+
+    let mut i = 0;
 
     // Video/Game loop
     'running: loop {
@@ -60,38 +69,38 @@ fn main() -> Result<(), String> {
 
         match dir {
             0 => {
-                x += 1;
-                y += 1
+                x += w_rec as i32;
+                y += h_rec as i32
             }
 
             1 => {
-                x -= 1;
-                y += 1
+                x -= w_rec as i32;
+                y += h_rec as i32
             }
 
             2 => {
-                x -= 1;
-                y -= 1
+                x -= w_rec as i32;
+                y -= h_rec as i32
             }
 
             3 => {
-                x += 1;
-                y -= 1
+                x += w_rec as i32;
+                y -= h_rec as i32
             }
             _ => {}
         }
 
-        if x > 50 || x < 0 || y > 50 || y < 0 {
-            x = 25;
-            y = 25;
-        }
+        x = (x + (width * w_rec) as i32) % (width * w_rec) as i32;
+        y = (y + (height * h_rec) as i32) % (height * h_rec) as i32;
 
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.draw_point(Point::new(x, y))?;
-        canvas.set_scale(20., 20.).expect("Coudl not rescalate");
+        canvas.set_draw_color(Color::RGB(255 - i, i, 255 - i));
+        canvas.fill_rect(Rect::new(x, y, w_rec, h_rec))?;
 
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1000u32));
+        ::std::thread::sleep(Duration::new(0, 10_000u32));
+
+        i = (i + 1) % 255;
     }
     Ok(())
 }
+
